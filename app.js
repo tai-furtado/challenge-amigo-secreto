c21e6860eafa45208adc84810b8a981675486c37
Tainá Furtado <tainacfurtado@gmail.com>
2025-02-09 20:52:47 -0300

    Criação do array de nomes

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema
+let listaDeNomes = []; // Cria um array vazio chamado listaDeNomes
